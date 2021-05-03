c7230a556a7ef5847b957b1a968378faf7b6e06e
Darbaz Ali <darbaz.me@gmail.com>
2021-05-03 15:01:51 +0300

    Point object using class syntax

--- oop/createPoint.js
+++ oop/createPoint.js
@@ -69,3 +69,60 @@ point.scale(2)
 
 console.log(point.getX())
 console.log(point.getY())
+
+// Point Constructor using class syntax
+class Point2 {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  get getX() {
+    return this.x
+  }
+
+  get getY() {
+    return this.y
+  }
+
+  /**
+   * @param {number} newX
+   */
+  set setX(newX) {
+    if (typeof newX === 'number') {
+      this.x = newX
+    } else {
+      throw new Error('Please provide a valid number')
+    }
+  }
+  /**
+   * @param {number} newY
+   */
+  set setY(newY) {
+    if (typeof newY === 'number') {
+      this.y = newY
+    } else {
+      throw new Error('Please provide a valid number')
+    }
+  }
+
+  translate(xPoint, yPoint) {
+    this.x += xPoint
+    this.y += yPoint
+  }
+
+  scale(factor) {
+    this.x *= factor
+    this.y *= factor
+  }
+}
+
+const point2 = new Point2(0, 0)
+point2.translate(4, 5)
+point2.scale(2)
+
+point2.setX = 22
+point2.setY = 33
+
+console.log(point2.getX)
+console.log(point2.getY)
