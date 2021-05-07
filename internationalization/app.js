045317896906cdb84b9d5fd8d978d4fe2affe393
Darbaz Ali <darbaz.me@gmail.com>
2021-05-07 16:35:34 +0300

    internationalization - formating numbers

--- internationalization/app.js
+++ internationalization/app.js
@@ -2,6 +2,17 @@
 
 /* 
     1) specifying a local
+
+    common lnaguage codes
+
+    Chinese => zh
+    Japanese => ja
+    Danish => da
+    Korean => ko
+    Dutch => du
+    English => en
+    French => fr
+    Spanish => es
 */
 const newYear = new Date()
 
@@ -10,4 +21,32 @@ console.log(newYear)
 console.log(newYear.toLocaleString('de')) // Denmark
 console.log(newYear.toLocaleString('ar')) // Arabic - Iraq
 console.log(newYear.toLocaleString('de')) // Germany
-console.log(newYear.toLocaleString('de', { timeZone: 'Asia/Tokyo' }))
+console.log(newYear.toLocaleString('ja', { timeZone: 'Asia/Tokyo' }))
+
+/* 
+    2) Formating Numbers
+    We con format a number to a particluar local using number.toLocalString('local')
+*/
+
+const number = 123456.78
+
+console.log(number.toLocaleString('de'))
+console.log(number.toLocaleString('ar'))
+console.log(number.toLocaleString('zh-Hans-CN'))
+console.log(number.toLocaleString('ko'))
+console.log(number.toLocaleString('sv'))
+
+// Alternatively, we can construct an instance of the Intl.NumberFormat class
+
+const formatter = new Intl.NumberFormat('de', {
+  style: 'currency',
+  currency: 'EUR',
+})
+
+const formatIraq = new Intl.NumberFormat('ar', {
+  style: 'currency',
+  currency: 'IQD',
+})
+
+console.log(formatter.format(number))
+console.log(formatIraq.format(number))
