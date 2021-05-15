fd1b8dde95f627e2910f1c3e5db6656b997e2e1c
Darbaz Ali <darbaz.me@gmail.com>
2021-05-15 13:35:05 +0300

    customizing toString method of Object class

--- metaProgramming/symbols.js
+++ metaProgramming/symbols.js
@@ -11,4 +11,30 @@ let obj = {
   [sym]: 'Symbol value',
 }
 
-console.log(sym.description)
+console.log(obj[sym])
+console.log(obj[sym2])
+
+/*===============================================================================
+Customizing toString
+================================================================================*/
+//  Costomizing toString() method of Object class
+
+const harry = { name: 'Harry Smith', salary: 10_000 }
+// by default harry.toString() yields [object Object]
+console.log(harry.toString()) // [object Object]
+
+// now, lets modify it with Symbol.toStringTag
+harry[Symbol.toStringTag] = 'Employee'
+console.log(harry.toString()) // [object Employee]
+
+// alternatively, you can set the property in the constructor
+class Employee {
+  constructor(name, salary) {
+    this[Symbol.toStringTag] = 'Employee'
+    this.name = name
+    this.salary = salary
+  }
+}
+
+const mac = new Employee('Mac Redford', 90_000)
+console.log(mac.toString()) // [object Employee]
